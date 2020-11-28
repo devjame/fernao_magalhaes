5d3e9c624e0ccddab1b817679309a4a6d07811bf
devjame <devjames79@gmail.com>
2020-11-28 19:32:26 +0000

    criar novo ecrã e nova cena de alistamento

--- game_gui/exemple/test.py
+++ game_gui/exemple/test.py
@@ -1,5 +1,6 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
+
 layout = """
 MDBoxLayout:
     orientation: 'vertical'
@@ -20,91 +21,82 @@ class TestAPP(MDApp):
         return Builder.load_string(layout)
 
 
-
 TestAPP().run()
-canvas.before:
-        Color:
-            rgba: 0, 0, 1, 1
-        Rectangle:
-            pos: self.pos
-            size: self.size
 
+
+# canvas.before:
+#         Color:
+#             rgba: 0, 0, 1, 1
+#         Rectangle:
+#             pos: self.pos
+#             size: self.size
+#
 
 ###################
-MDBoxLayout:
-            MDLabel:
-                canvas.before:
-                    Color:
-                        rgba: 1, 0, 0, 1
-                    Rectangle:
-                        pos: self.pos
-                        size: self.size
-                size_hint_y: None
-                height: 60
-                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-                bold: True
-                font_size: 20
-                halign: "center"
 
-            MDGridLayout:
-                spacing: 10
-                cols: 1 if Window.size[0] <= 460 else 2
-                size_hint_y: None
-                height:
-                MDLabel:
-                    text: "Donec mollis viverra lacinia. Sed in purus ligula. Praesent et imperdiet felis, a ornare eros. Proin sapien ipsum, fringilla nec ornare non, gravida non purus. Sed consequat orci a risus suscipit convallis"
-                    font_size: 16
-                    size_hint_y: None
-                    height: 150
-
-                MDLabel:
-                    canvas.before:
-                        Color:
-                            rgba: 1, 0, 0, 1
-                        Rectangle:
-                            pos: self.pos
-                            size: self.size
-                    text: str(Window.size)
-                    bold: True
-                    font_size: 20
-                    halign: "center"
-                    size_hint_y: None
-                    height:
-                    size_hint_x: 1 if Window.size[0] <= 460 else .2
-
-                MDLabel:
-                    canvas.before:
-                        Color:
-                            rgba: 0, 0, 1, 1
-                        Rectangle:
-                            pos: self.pos
-                            size: self.size
-                    size_hint_y: None
-                    height: 200
-                    text: "Mauris et lorem augue. Pellentesque euismod, augue ut tristique tincidunt, sem velit porta nibh, eget condimentum ipsum risus eu felis. Morbi non justo blandit, tempor nisi eu, vehicula massa. Ut aliquet nunc viverra, egestas nisl id, pharetra purus. Vestibulum imperdiet sem varius, ornare tellus eu, aliquam massa."
-                MDLabel:
-                    canvas.before:
-                        Color:
-                            rgba: 0, 1, 0, 1
-                        Rectangle:
-                            pos: self.pos
-                            size: self.size
-                    size_hint_y: None
-                    height: 200
-                    text: "Cras tristique dapibus enim at faucibus. Nam facilisis mi id mi bibendum, nec eleifend ligula lacinia." *5
 
 ###############################
 class BuildRequester(BoxLayout):
     chkref = {}
-    def on_checkbox_active(self,chkbox,value):
+
+    def on_checkbox_active(self, chkbox, value):
         self.ids.label2.text = 'Selected ' + self.chkref[chkbox]
+
     def __init__(self, **kwargs):
-        super(BuildRequester,self).__init__(**kwargs)
-        prods = [' B_0003',' B_0007',' B_0008', ' B_0200']
+        super(BuildRequester, self).__init__(**kwargs)
+        prods = [' B_0003', ' B_0007', ' B_0008', ' B_0200']
 
         for i in range(4):
-            self.add_widget(Label(text=prods[i],italic=True,bold=True))
-            chkbox = CheckBox(group='1',color=[0.1,1,0,4])
+            self.ids.content_grid.add_widget(Label(text=prods[i], italic=True, bold=True id="lbl_indedx_{}".format(i)))
+            chkbox = CheckBox(group='1', color=[0.1, 1, 0, 4],id="cb_indedx_{}".format(i))
             chkbox.bind(active=self.on_checkbox_active)
-            self.add_widget( chkbox)
-            self.chkref[chkbox]= prods[i]
+            self.add_widget(chkbox)
+            self.chkref[chkbox] = prods[i]
+
+MDCheckbox:
+                    id: cb_index_1
+                    size_hint: None, None
+                    size: "48dp", "48dp"
+                    group: "1"
+                    on_active: app.root.ids.quiz_screen.on_checkbox_active(*args)
+
+                MDLabel:
+                    id: lbl_index_1
+                    text: "Mauris et lorem augue. Pellentesque euismod, augue ut tristique tincidunt"
+                    font_size: 14
+
+                MDCheckbox:
+                    id: cb_index_2
+                    size_hint: None, None
+                    size: "48dp", "48dp"
+                    group: "1"
+                    on_active: app.root.ids.quiz_screen.on_checkbox_active(*args)
+
+                MDLabel:
+                    id: lbl_index_2
+                    text: "Mauris et lorem augue. Pellentesque euismod, augue ut tristique tincidunt"
+                    font_size: 14
+
+                MDCheckbox:
+                    id: cb_index_3
+                    size_hint: None, None
+                    size: "48dp", "48dp"
+                    group: "1"
+                    on_active: app.root.ids.quiz_screen.on_checkbox_active(*args)
+
+                MDLabel:
+                    id: lbl_index_3
+                    text: "Mauris et lorem augue. Pellentesque euismod, augue ut tristique tincidunt"
+                    font_size: 14
+
+                MDCheckbox:
+                    id: cb_index_4
+                    size_hint: None, None
+                    size: "48dp", "48dp"
+                    group: "1"
+                    on_active: app.root.ids.quiz_screen.on_checkbox_active(*args)
+
+                MDLabel:
+                    id: lbl_index_4
+                    text: "Mauris et lorem augue. Pellentesque euismod, augue ut tristique tincidunt"
+                    font_size: 14
--- game_gui/main.py
+++ game_gui/main.py
@@ -1,33 +1,67 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
-
-
+from kivymd.uix.selectioncontrol import MDCheckbox
+from kivymd.uix.label import MDLabel
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.gridlayout import MDGridLayout
 from kivy.core.window import Window
+from kivy.properties import ObjectProperty
 
 Window.size = (320, 800)
 
 
+class Alistamento(Screen):
+    text_field = ObjectProperty()
+    ingreso = ObjectProperty()
+    info_partida = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super(Alistamento, self).__init__(**kwargs)
+
+    def mostrar_documento(self, *args):
+        documento = f"Eu, {self.text_field.text} comprometo-me a ingressar nesta viagem sobre o commando do capitão Fernão de Maglhães, visando " \
+                    "descobrir uma nova rota para as ilhas Molucas."
+        self.ingreso.text = documento
+        self.info_partida.text = "-> Dia da Partida: 8  de Agosto de 1519.\n -> Horas: 06:00H\n -> Porto de Sivilha"
+
+       
 class HomeScreen(Screen):
     pass
 
 
 class QuizScreen(Screen):
+    pass
+
+
+class BoxOpcao(MDGridLayout):
+    chkref = {}
+
     def __init__(self, **kwargs):
-        super(QuizScreen, self).__init__(**kwargs)
+        super(BoxOpcao, self).__init__(**kwargs)
+        self.dialog = None
+        self.opcao = [
+            "Curioso para saber mais, dirijeste à mesa onde falam da grande viagem e é te proposto que te alistes nesta\
+            demanda dirigida por Frenão de Magalhães, um capitão Português",
+            "Continuas a beber a tua cerveja e no dia seguinte voltas à tua rotina de marinheiro miserável até ao fim \
+            dos teus dias",
+        ]
+        for i in range(len(self.opcao)):
+            self.lbl = MDLabel(text=self.opcao[i], font_size=14)
+            self.chkbox = MDCheckbox(group='opçao', color=[0.1, 1, 0, 4], size_hint=(None, None), size=("48dp", "48dp"))
+            self.chkbox.bind(active=self.on_checkbox_active)
+            self.add_widget(self.chkbox)
+            self.add_widget(self.lbl)
+            self.chkref[self.chkbox] = self.opcao[i]
 
     def on_checkbox_active(self, checkbox, value):
         if value:
-            print('The checkbox', checkbox, 'is inactive', 'and', checkbox.state, 'state')
-            print(self.ids.lbl_index_1.text)
-        else:
-            print('The checkbox', checkbox, 'is inactive', 'and', checkbox.state, 'state')
+            print(f"{checkbox} -> {self.chkref[checkbox]}")
 
 
 class MainApp(MDApp):
 
     def build(self):
-        print(self.root.ids.quiz_screen.ids)
         return Builder.load_file("main.kv")
 
 
