831f0ded7a4337b0fe0b2465af316f1840f77818
devjame <devjames79@gmail.com>
2020-11-28 21:03:14 +0000

    fix: quiz checkbox

--- game_gui/main.py
+++ game_gui/main.py
@@ -3,8 +3,8 @@ from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivymd.uix.selectioncontrol import MDCheckbox
 from kivymd.uix.label import MDLabel
-from kivymd.uix.textfield import MDTextField
 from kivymd.uix.gridlayout import MDGridLayout
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.core.window import Window
 from kivy.properties import ObjectProperty
 
@@ -25,7 +25,7 @@ class Alistamento(Screen):
         self.ingreso.text = documento
         self.info_partida.text = "-> Dia da Partida: 8  de Agosto de 1519.\n -> Horas: 06:00H\n -> Porto de Sivilha"
 
-       
+
 class HomeScreen(Screen):
     pass
 
@@ -35,24 +35,29 @@ class QuizScreen(Screen):
 
 
 class BoxOpcao(MDGridLayout):
+
     chkref = {}
 
     def __init__(self, **kwargs):
         super(BoxOpcao, self).__init__(**kwargs)
         self.dialog = None
         self.opcao = [
-            "Curioso para saber mais, dirijeste à mesa onde falam da grande viagem e é te proposto que te alistes nesta\
-            demanda dirigida por Frenão de Magalhães, um capitão Português",
-            "Continuas a beber a tua cerveja e no dia seguinte voltas à tua rotina de marinheiro miserável até ao fim \
-            dos teus dias",
+            "Curioso para saber mais, dirijeste à mesa onde falam da grande viagem e é te proposto que te alistes nesta demanda dirigida por Frenão de Magalhães, um capitão Português",
+            "Continuas a beber a tua cerveja e no dia seguinte voltas à tua rotina de marinheiro miserável até ao fim dos teus dias",
         ]
         for i in range(len(self.opcao)):
-            self.lbl = MDLabel(text=self.opcao[i], font_size=14)
-            self.chkbox = MDCheckbox(group='opçao', color=[0.1, 1, 0, 4], size_hint=(None, None), size=("48dp", "48dp"))
+            self.lbl = MDLabel(text=self.opcao[i], font_size=14, valign="middle", size_hint_x=.9)
+            self.chkbox = MDCheckbox(group='opçao', color=[0.1, 1, 0, 4], size_hint=(None, None), size=("48dp", "48dp"),
+                                     pos_hint={"y": .4})
             self.chkbox.bind(active=self.on_checkbox_active)
-            self.add_widget(self.chkbox)
+
+            self.container = MDBoxLayout(size_hint_x=.1)
+            self.container.add_widget(self.chkbox)
+            self.add_widget(self.container)
             self.add_widget(self.lbl)
+
             self.chkref[self.chkbox] = self.opcao[i]
+            print(self.chkbox.y)
 
     def on_checkbox_active(self, checkbox, value):
         if value:
